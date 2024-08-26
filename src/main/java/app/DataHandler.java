fa3ad2abf32ca14ae7ab3c6cdb2fd1b2e01ac5b8
Daria <dashalokomoko30@gmail.com>
2024-08-26 06:11:27 -0400

    Initial commit

---
+++ src/main/java/app/DataHandler.java
@@ -0,0 +1,16 @@
+package app;
+
+
+public class DataHandler <T>{
+
+    public String dataHandler(T[] items){
+        StringBuilder sb = new StringBuilder();
+        int count=0;
+
+        for (T item: items) {
+            count++;
+            sb.append(String.format("(%d)%s" , count, item));
+        }
+        return sb.toString();
+    }
+}
